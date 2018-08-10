20edc76447cf344a58a3d203697794dfc0ad16a5
Bill Leddy <bill@williesworkshop.net>
2018-08-10 09:16:46 -0700

    Show current stats in get_data response

--- jump/views/jump.py
+++ jump/views/jump.py
@@ -1,6 +1,7 @@
 from flask import request, session, g, redirect, url_for, abort, \
      render_template, flash, Blueprint
 from users.admin import login_required, table_access_required
+from jump.models import Bike, Sighting, Trip
 
 mod = Blueprint('jump',__name__, template_folder='../templates', url_prefix='/jump')
 
@@ -15,6 +16,15 @@ def get_data():
     setExits()
     from jump.get_data import get_jump_data
     result = get_jump_data()
-    #flash("Jump Data Updated. - " + result)
-    return render_template('jump_data_response.html', result=result)
+    data = {}
+    data['bikes'] = get_result_count(Bike(g.db).select())
+    data['sightings'] = get_result_count(Sighting(g.db).select())
+    data['trips'] = get_result_count(Trip(g.db).select())
+    return render_template('jump_data_response.html', result=result, data=data)
+    
+def get_result_count(rec):
+    if not rec:
+        return 0
+    else:
+        return len(rec)
     
